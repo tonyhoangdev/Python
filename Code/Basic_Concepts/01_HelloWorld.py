05e882ef4ba770d15048d05ba714859a0023a625
Tony Hoang <hoangtrongminh304@gmail.com>
2017-07-08 19:26:24 +0700

    Init

---
+++ Code/Basic_Concepts/01_HelloWorld.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+    @author: MinhHT3
+    @brief: First program
+"""
+
+print('Hello world')
+print('Hello world')
+print('Spam and eggs...')
+print(
+    """
+    Spam and
+    newline...
+    """
+    )
---
+++ Code/Basic_Concepts/02_SimpleInOut.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+    @author: MinhHT3
+    @brief: Simple Operations
+"""
+
+print(1+1)
+print("Hello\nWorld!")
+print('print("print")')
+
+var1 = 5
+# input('Enter something please: ')
+print(var1)
